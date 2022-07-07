3ad1f1c03c9c25911649a06c0306ec23a5842cc4
akmal09 <akmal.119140203@student.itera.ac.id>
2022-07-07 10:21:11 +0600

    courier check order response success

--- controllers/machineController.js
+++ controllers/machineController.js
@@ -6,37 +6,47 @@ const axios = require("axios");
 const fetch = require('node-fetch');
 // import axios, * as others from 'axios';
 // const { response } = require("express");
-
+const responseData = {
+    code:"",
+    latency:"",
+    message:""
+}
 
 const getPackageNumber = async (req, res) => {
     const packagesNumber = req.body;
-    // request({
-    //     method:"POST",
-    //     url : "https://api.github.com/users/crymlll"
-    // }, function(error, response, body){
-    //     console.log(body)
-    // })
-    axios("http://127.0.0.1:8000/locker/check/order",{
-        method:"POST",
-        headers:{
-            "Content-Type" : "application/json",
-            "x_terminal_id" : "212312",
-            "x_token" : "se12jsnpo"
-        }
-    }).then(res => {
-        console.log(res.data)
-    })
-    .catch(error =>console.log(error))
+    // console.log(packagesNumber.package_id)
+    if(packagesNumber.package_id == undefined || packagesNumber.package_id == null ||packagesNumber.package_id ==""){
+        res.send({response : "null"})
+    }else{
+        axios("http://127.0.0.1:8000/locker/check/order",{
+            method:"POST",
+            headers:{
+                "Content-Type" : "application/json",
+                "x_terminal_id" : "212312",
+                "x_token" : "se12jsnpo"
+            },
+            body:{
+                "package_number":packagesNumber.package_id
+            }
+        }).then(responseApi => {
+            // console.log(packagesNumber.body.package_id)
+            responseData.code = 200
+            responseData.latency=13132
+            responseData.message = "OK"
+            res.send({
+                response:responseData,
+                data : {
+                    code : responseApi.data.response.code,
+                    message : responseApi.data.response.message,
+                    data : responseApi.data.data
+                }
+            })
+        })
+        .catch(error =>console.log(error))
+    }
+    
 
-    // fetch("http://127.0.0.1:8000/check/order",{
-    //     method : 'GET',
-    //     headers:{
-    //         'x_terminal_id':'212312',
-    //         'x_token':'se12jsnpo'
-    //     }
-    // }).then((res)=>
-    //     console.log(res.json())
-    // )
+    
     // await Package.findOne({where: { pahttp://127.0.0.1:8000/check/orderckage_number : packagesNumber} }).then((data) => {
     //     // console.log(data)
     // })
---
+++ helper/response.js
@@ -0,0 +1,11 @@
+const responseObject = function(data){
+    return {
+        code:data,
+        latency:"asdja",
+        message:"asdasfm",
+    }
+}
+
+module.exports = {
+    responseData : responseObject
+}
