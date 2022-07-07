9e3d792dc6612292ad0e2790782306fdd51e3259
Daffa Praramadhana <daffapraramadhana@gmail.com>
2022-07-07 16:35:34 +0700

    dvsdfsgfs

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-if(process.env.NODE_ENV == "production"){
-    require('dotenv').config()
+if (process.env.NODE_ENV == "production") {
+  require("dotenv").config();
 }
 
-const sequelize = require("./database/dbconfig")
+const sequelize = require("./database/dbconfig");
 const Package = require("./models/Package");
-const cors = require("cors")
+const cors = require("cors");
 
 // sequelize.sync({ force: true }).then(async () => {
 //     console.log()
 // })
 
-
-
 // sequelize.sync({ force: true})
-const express = require("express")
-const routes = require("./routes/route")
+const express = require("express");
+const routes = require("./routes/route");
 
 const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin : "http://localhost:3005",
-    optionsSuccessStatus: 200
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.get("/", (req, resp) => resp.send("application is up and running"))
+app.get("/", (req, resp) => resp.send("application is up and running"));
 
-app.use("/service", routes.routes)
+app.use("/service", routes.routes);
 
 const PORT = process.env.PORT | 3005;
 app.listen(PORT, () => {
-    console.log(`Service endpoint = http://localhost:${PORT}`)
+  console.log(`Service endpoint = http://localhost:${PORT}`);
 });
